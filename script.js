c679efcda08f99e607effa8ea7e99bc10535fe0c
khuongduy1484 <khuongduy1484@gmail.com>
2019-04-19 14:16:01 +0700

    comit

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let a =[];
 for (let i = 0;i<100;i++){
-    a[i] = Math.floor(Math.random()*100);
+    a[i] = Math.floor(Math.random()*101);
 }
-document.write(a);
+let arr ='';
+for (let i = 0,j=1;i<a.length && j<=10;i++,j++){
+    arr+=a[i]+' ';
+    if (j===10) {
+        arr+='</br>';
+        j=1
+    }
+}document.getElementById('number').innerHTML = arr;
+
 function maxinum_value(arr,value) {
-    let max = [0];
+    let max = arr[0];
     for (let i =0;i<arr.length;i++) {
         if (max<arr[i]){
             max = a[i];
@@ -16,7 +24,7 @@ document.write("<br/>");
 maxinum_value(a);
 
 function mininum_value(arr) {
-    let min = [0];
+    let min = arr[0];
     for (let i = 0; i < arr.length; i++){
         if (min>arr[i]){
             min = a[i];
